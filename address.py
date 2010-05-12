d64e50e0ac2e5fd980cdd9554f31c23d94647bd1
Jay Moorthi <jaymoorthi@me.com>
2010-05-12 15:33:23 -0700

    Auto-populate properties and labelmapping.

--- address.py
+++ address.py
@@ -1,18 +1,51 @@
+#!/usr/bin/python
+
 import sqlite3
 import os
 import sys
 import re
 import collections
 
+if len(sys.argv) < 2:
+    sys.stderr.write('usage: %s AddressBook.sqlitedb\n' % sys.argv[0])
+    sys.exit(-1)
+
 dbfn = sys.argv[1]
 
 conn = sqlite3.connect(dbfn)
 c = conn.cursor()
 
-# TODO: auto-populate properties
-# TODO: auto-number ABMultiValueLabel
+# TODO: error handling
+# TODO: make backups
 
-c.execute('select First, Last, Organization, properties.property, ABMultiValue.value, ABMultiValueLabel.value as label from ABPerson, ABMultiValue, ABMultiValueLabel,properties where ROWID=record_id and ABMultiValue.label=ABMultiValueLabel.id and ABMultiValue.property = properties.id and ABMultiValue.value not null')
+properties = {
+    3: 'phone',
+    4: 'email',
+}
+
+c.execute('drop table if exists properties')
+c.execute('create table properties ("id" INTEGER, "property" TEXT)')
+
+for id, property in properties.iteritems():
+    c.execute('insert into properties (id, property) VALUES (?, ?)', (id, property)) 
+
+conn.commit()
+
+c.execute('drop table if exists labelmapping')
+c.execute('create table labelmapping ("id" INTEGER, "label" TEXT)')
+
+labels = conn.cursor()
+labels.execute('select value from ABMultiValueLabel')
+id = 1
+for row in labels:
+    v = row[0]
+    sys.stderr.write("writing labelmapping: %d %s\n" % (id, v))
+    c.execute('insert into labelmapping (id, label) VALUES (?, ?)', (id, v))
+    id += 1
+
+conn.commit()
+
+c.execute('select First, Last, Organization, properties.property, ABMultiValue.value, labelmapping.label as label from ABPerson, ABMultiValue, labelmapping, properties where ROWID=record_id and ABMultiValue.label=labelmapping.id and ABMultiValue.property = properties.id and ABMultiValue.value not null')
 
 entries = {}
 columns = ['first', 'last', 'organization']
